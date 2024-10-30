4d41b0e2083b055927a6e6cf58426f0f3159ddd0
Aziz <khazpam.11@gmail.com>
2024-10-30 15:08:59 +0700

    adding kafka features into AddUserHandler

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -1,16 +1,29 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/azizpambayun/ecommerce-micro/models"
+	"github.com/segmentio/kafka-go"
 )
 
 var users = []models.User{
 	{ID:1, Username: "ilham", Email: "ilham@windah.com", Password: "hashedpassword", Created: time.Now()},
 }
+
+// Kafka writer to send messages
+var kafkaWriter = &kafka.Writer{
+	Addr: kafka.TCP("localhost:9902"),
+	Topic: "user-events",
+	Balancer: &kafka.LeastBytes{},
+}
+
+
 
 // GetUserHandler handles fetching user information
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +59,18 @@ func AddUserHandler(w http.ResponseWriter, r *http.Request) {
 	newUser.Created = time.Now()
 	users = append(users, newUser)
 
+
+	// Publish the event to Kafka
+	msg := fmt.Sprintf("New user added: %s", newUser.Username)
+	err := kafkaWriter.WriteMessages(context.Background(), kafka.Message{
+		Key: []byte(fmt.Sprintf("user-%d", newUser.ID)),
+		Value: []byte(msg),
+	}) 
+	if err != nil {
+		log.Printf("could not write message to kafka: %v", err)
+	}
+
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newUser)
+
 }
